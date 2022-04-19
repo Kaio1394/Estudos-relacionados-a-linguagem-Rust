c883c20667b28ad6a9bc5958da758563d8ea047d
Kaio <kaio.santiago.13@hotmail.com>
2022-04-19 16:21:11 -0300

    Slice

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -9,7 +9,6 @@ fn main() {
     println!("{}", array.len());
     println!("{:?}", array[0]); // pegando índice de um array
     println!("{:?}", numbers);
-    
     // somatória dos elementos de um array
     let mut soma = 0;
     for i in 0..numbers.len() {
@@ -24,5 +23,17 @@ fn main() {
     let mut primes: Vec<Vec<i32>> = Vec::new();
     primes = vec![vec![2, 3, 5], vec![2, 3, 5]];
     primes.push(vec![1, 2]);
-    println!("{:?}", primes[0]);
+    // Similar setup da estrutura dos arrays 
+    // [Tipo do elemento(int, bool, int, etc...); N_vezes]
+    let vetor = vec![2; 10];  
+    println!("{:?}", primes);
+    println!("{:?}", vetor);
+    primes.remove(0); // Removendo um elemento do vetor pelo indice
+    println!("{:?}", primes);
+
+    // Slice
+    let array_teste = [0, 1, 2, 3, 4, 5, 6];
+    let slice_array = &array_teste[0..3];
+    println!("{:?}", slice_array);
+    
 }
